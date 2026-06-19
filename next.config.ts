452ac74f24ca2f8d49e55015ab8594b55f41a2db
AlexandruJungean <alex.jungean@gmail.com>
2026-06-19 14:46:48 +0300

    fixing 'npm run lint'

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.emeoutlookmag.com",
+      },
+      {
+        protocol: "https",
+        hostname: "static.toprecepty.cz",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
